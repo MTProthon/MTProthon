9359a576e460392a3a45c72c3f08001190f01ba0
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2025-11-03 01:57:00 +0330

    Add `core.py`

--- mtprothon/type_language/__init__.py
+++ mtprothon/type_language/__init__.py
@@ -2,3 +2,4 @@ from .tlobject import TLObject
 from .primitives import Int, Long, Int128, Int256, Double, Bool, Bytes, String, Vector
 from .types import ResPQ
 from .functions import ReqPqMulti
+from .core import Message
---
+++ mtprothon/type_language/core.py
@@ -0,0 +1,29 @@
+from io import BytesIO
+
+from .tlobject import TLObject
+from .primitives import Long, Int
+
+
+class Message(TLObject):
+    def __init__(self, auth_key_id: bytes, id: int, length: int, data: bytes):
+        self.auth_key_id = auth_key_id
+        self.id = id
+        self.length = length
+        self.data = data
+
+    def serialize(self):
+        return self.auth_key_id + Long(self.id).serialize() + Int(self.length).serialize() + self.data
+
+    @classmethod
+    def deserialize(cls, data: BytesIO):
+        auth_key_id = data.read(8)
+
+        id = Long.deserialize(data)
+
+        length = Int.deserialize(data)
+
+        payload = data.read(length)
+        if len(payload) < length:
+            raise ValueError(f"Expected {length} bytes, got {len(payload)}")
+
+        return cls(auth_key_id, id, length, payload)
