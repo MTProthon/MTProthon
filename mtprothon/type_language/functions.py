8bdbd6b347c00a0486f72e46fd5667038b5b2a3e
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2025-11-07 01:53:39 +0330

    Improve code quality

--- mtprothon/type_language/functions.py
+++ mtprothon/type_language/functions.py
@@ -1,12 +1,11 @@
 from .tlobject import TLObject
+from .primitives import Int128
 
 
 class ReqPqMulti(TLObject):
     ID = 0xbe7e8ef1
 
-    def __init__(self, nonce):
-        self.nonce = nonce
+    tl_types = [Int128]
 
-    def serialize(self) -> bytes:
-        constructor = self.ID.to_bytes(length=4, byteorder="little")
-        return constructor + self.nonce
+    def __init__(self, nonce: int):
+        self.nonce = nonce
