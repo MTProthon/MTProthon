4cf7cb0da0d6979d48f425de08ce5e93b7231494
SajjadAlipour2006 <136921317+SajjadAlipour2006@users.noreply.github.com>
2025-11-07 01:34:43 +0330

    Add comprehensive `deserialize` method to `TLObject`

--- mtprothon/type_language/tlobject.py
+++ mtprothon/type_language/tlobject.py
@@ -1,3 +1,26 @@
+from io import BytesIO
+
+
 class TLObject:
-    def __init__(self, value):
-        self.value = value
+    ID: int
+
+    tl_types = []
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def get_tl_types(cls):
+        return cls.tl_types
+
+    def get_values(self):
+        return self.__dict__.values()
+
+    @classmethod
+    def deserialize(cls, data: BytesIO) -> "TLObject":
+        constructor = data.read(4)
+
+        if constructor != cls.ID.to_bytes(length=4, byteorder="little"):
+            raise ValueError(f"Expected {TLObject.__name__} constructor, got {constructor.hex()}")
+
+        return cls(*[t.deserialize(data) for t in cls.get_tl_types()])
--- mtprothon/type_language/types.py
+++ mtprothon/type_language/types.py
@@ -1,31 +1,16 @@
+from typing import List
+
 from .tlobject import TLObject
-from .primitives import Bytes, Vector, Long
+from .primitives import Bytes, Vector, Long, Int128
 
 
 class ResPQ(TLObject):
     ID = 0x05162463
 
-    def __init__(self, nonce, server_nonce, pq, fingerprints):
+    tl_types = [Int128, Int128, Bytes, Vector[Long]]
+
+    def __init__(self, nonce: int, server_nonce: int, pq: bytes, fingerprints: List[int]):
         self.nonce = nonce
         self.server_nonce = server_nonce
         self.pq = pq
         self.fingerprints = fingerprints
-
-    @classmethod
-    def deserialize(cls, data):
-        constructor = data.read(4)
-        expected = bytes.fromhex('63241605')  # 05162463 in little-endian
-        if constructor != expected:
-            raise ValueError(f"Expected resPQ constructor, got {constructor.hex()}")
-
-        nonce = data.read(16)
-        if len(nonce) < 16:
-            raise ValueError("Not enough data for nonce")
-
-        server_nonce = data.read(16)
-        if len(server_nonce) < 16:
-            raise ValueError("Not enough data for server_nonce")
-
-        pq = Bytes.deserialize(data)
-        fingerprints = Vector.deserialize(data, Long)
-        return cls(nonce, server_nonce, pq, fingerprints)
